8817cdb93e284bc5f80477592040f5283d15f3f2
sumaiya7121 <sumaiya7121@gmail.com>
2022-11-08 13:54:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const app= express();
+require('dotenv').config();
+const port =process.env.PORT || 5000;
+
+//middle wires
+app.use(cors());
+app.use(express.json());
+
+//mongodb
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@project1.puheqno.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/',(req,res)=>{
+    res.send('Server is running')
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
