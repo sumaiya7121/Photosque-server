d3368bc4863f5e5fc470d1fcc2ccd79dd99dc684
sumaiya7121 <sumaiya7121@gmail.com>
2022-11-10 17:11:19 +0600

    getOperation

--- index.js
+++ index.js
@@ -10,16 +10,121 @@ app.use(express.json());
 
 //mongodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@project1.puheqno.mongodb.net/?retryWrites=true&w=majority`;
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+
+async function run(){
+
+
+try{
+
+const serviceCollection = client.db('photosque').collection('services');
+const reviewCollection = client.db('photosque').collection('review');
+
+app.get('/service',async(req,res)=>{
+
+const query ={}
+const limit = 3;
+const cursor =serviceCollection.find(query).limit(limit);
+const services =await cursor.toArray();
+res.send(services);
+
+
 });
+
+app.get('/services',async(req,res)=>{
+
+const query ={}
+
+const cursor =serviceCollection.find(query);
+const services =await cursor.toArray();
+res.send(services);
+
+
+});
+
+
+
+app.get('/services/:id',async(req,res)=>{
+
+const id= req.params.id;
+
+const query ={_id: ObjectId(id)}
+const service = await serviceCollection.findOne(query)
+res.send(service);
+
+});
+
+
+app.post('/services',async (req,res)=>{
+
+    const service=req.body;
+    const result= await serviceCollection.insertOne(service);
+    res.send(result);
+
+})
+
+
+//reviews
+app.get('/myreviews',async(req,res)=>{
+let query={};
+
+    if
+    
+    (req.query.email){
+    query={
+    email:req.query.email
+      }
+    }
+
+
+const cursor= reviewCollection.find(query);
+const review = await cursor.toArray();
+res.send(review);
+
+
+
+});
+
+app.post('/myreviews', async(req,res)=>{
+
+    const review=req.body;
+    const result =await reviewCollection.insertOne(review);
+    res.send(result);
+
+
+});
+
+//delete
+
+app.delete('/myreviews/:id',async(req,res)=>{
+
+const id =req.params.id;
+const query={_id: ObjectId(id)};
+const result = await reviewCollection.deleteOne(query);
+res.send(result);
+
+
+})
+
+
+
+
+}
+
+
+
+
+finally{
+
+
+}
+}
+run().catch(err=>console.log(err));
 
 
 app.get('/',(req,res)=>{
